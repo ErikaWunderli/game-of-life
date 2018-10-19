0ecc74ef2cd6239d376da4176ea22f1a6f547c4a
Erika Wunderli-Vallai <vallai.erika@gmail.com>
2018-10-19 14:23:59 +0200

    Add files via upload

---
+++ public/import-files.py
@@ -0,0 +1,10 @@
+import lifextractor.py
+
+def main ():
+    f=open("acorn.lif", "r")
+        if f.mode == 'r':
+            contents =f.read() 
+            
+if __name__=="__main__":
+    main()
+      
